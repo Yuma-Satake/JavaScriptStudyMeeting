4aaab9b062194379f838c73a6585597b5d7b5a75
Yuma Satake <satous.y@icloud.com>
2023-05-24 21:25:09 +0900

    feat: start/end

---
+++ start/index.js
@@ -0,0 +1,12 @@
+function bonnnnnnnn() {
+  console.log("bonnnnnnnn");
+}
+
+function clickKey(number) {
+  // 1.HTML要素のidがdispのHTMLを取得する
+  // 2.dispHTMLの中身を表示する
+  // 3.dispHTMLのinnerTextを表示する
+  // 4.dispHTMLのinnerTextをnumberにする
+  // 5.dispNumにnumber（入力値）を足す;
+  // 6.dispHTMLのinnerTextをdispNumにする
+}
